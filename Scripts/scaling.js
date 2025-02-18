e8f8cde822de2087846c30c15110e518fdd3a034
strobobrain <parkschaden@gmail.com>
2025-02-18 22:56:11 +0100

    build

---
+++ Scripts/scaling.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function () {
+    function scaleViewport() {
+      const container = document.querySelector('.appRoot');
+      const viewportWidth = window.innerWidth;
+      const viewportHeight = window.innerHeight;
+  
+      
+      const scaleX = viewportWidth / 1080;
+      const scaleY = viewportHeight / 1920;
+      const scale = Math.min(scaleX, scaleY);
+  
+      // Apply scaling
+      container.style.transform = `scale(${scale})`;
+      container.style.transformOrigin = 'top left';
+    }
+  
+    // Initial scale
+    scaleViewport();
+  
+    // Rescale on window resize
+    window.addEventListener('resize', scaleViewport);
+  });
