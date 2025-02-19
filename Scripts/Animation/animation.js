73546b782f042061ce718ffc7362c56ac07f2ef1
strobobrain <parkschaden@gmail.com>
2025-02-19 09:29:24 +0100

    organised image resources

--- Scripts/Animation/animation.js
+++ Scripts/Animation/animation.js
@@ -1,34 +1,27 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Start when the document is loaded
-    
-    const canvas = document.getElementById('animationCanvas');
-    const context = canvas.getContext('2d');
-    const frameRate = 2; // frames per second (slower)
-    const frameCount = 2; // total number of frames
-    let currentFrame = 0;
+// Parent div for animation
+const parentDiv = document.getElementById('centerContent');
+var frameRate = 2;
+var frameCount = 4;
+var imagesPathArray = generateImagePathArray("Crakuma_01", frameCount);
 
-    function loadFrame(index) {
-        return new Promise((resolve, reject) => {
-            const img = new Image();
-            img.src = `AvatarResources/TestTim/Tim${index}.svg`; // Adjust the path and naming convention as needed
-            img.onload = () => resolve(img);
-            img.onerror = reject;
-        });
+// return the path to the frame with the name
+function generateFrameImagePath(name, index) {
+    return "/ImageResources/Crakumas/" + name + "/frame_" + index +".png";
+}
+
+function generateImagePathArray(name, frameCount) {
+    var images = [];
+    for (var i = 0; i < frameCount; i++) {
+        images.push(generateFrameImagePath(name, i));
     }
+    return images;
+}
 
-    async function animate() {
-        try {
-            while (true) {
-                const img = await loadFrame(currentFrame % frameCount);
-                context.clearRect(0, 0, canvas.width, canvas.height); // Clear previous frame
-                context.drawImage(img, 0, 0, canvas.width, canvas.height); // Draw new frame
-                currentFrame = (currentFrame + 1) % frameCount; // Loop the frame count
-                await new Promise(resolve => setTimeout(resolve, 1000 / frameRate)); // Adjust the delay
-            }
-        } catch (error) {
-            console.error('Error loading frame:', error);
-        }
+function addChildrenToParentDiv(){
+    for (var i = 0; i < frameCount; i++) {
+        var img = document.createElement('img');
+        img.src = imagesPathArray[i];
+        parentDiv.appendChild(img);
     }
+}
 
-    animate();
-});
