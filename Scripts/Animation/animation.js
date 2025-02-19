4e9e8c8f9aae42d96fce30f4be82e754218ff652
strobobrain <parkschaden@gmail.com>
2025-02-19 10:22:27 +0100

    work in progress animation

--- Scripts/Animation/animation.js
+++ Scripts/Animation/animation.js
@@ -1,27 +1,52 @@
-// Parent div for animation
-const parentDiv = document.getElementById('centerContent');
-var frameRate = 2;
-var frameCount = 4;
-var imagesPathArray = generateImagePathArray("Crakuma_01", frameCount);
-
-// return the path to the frame with the name
-function generateFrameImagePath(name, index) {
-    return "/ImageResources/Crakumas/" + name + "/frame_" + index +".png";
-}
-
-function generateImagePathArray(name, frameCount) {
-    var images = [];
-    for (var i = 0; i < frameCount; i++) {
-        images.push(generateFrameImagePath(name, i));
+document.addEventListener('DOMContentLoaded', () => {
+    const parentDiv = document.getElementById('centerContent');
+    if (!parentDiv) {
+        console.error('Parent div not found');
+        return;
     }
-    return images;
-}
 
-function addChildrenToParentDiv(){
-    for (var i = 0; i < frameCount; i++) {
-        var img = document.createElement('img');
-        img.src = imagesPathArray[i];
-        parentDiv.appendChild(img);
+    const frameRate = 2;
+    const frameCount = 4;
+    const avatarName = "Crakuma_01";
+    let imagesPathArray;
+
+    // Return the path to the frame with the name
+    function generateFrameImagePath(name, index) {
+        return "/ImageResources/Crakumas/" + name + "/frame_" + index + ".png";
     }
-}
+
+    function generateImagePathArray(name, frameCount) {
+        const images = [];
+        for (let i = 0; i < frameCount; i++) {
+            images.push(generateFrameImagePath(name, i));
+        }
+        return images;
+    }
+
+    function addChildrenToParentDiv() {
+        // Delete all children
+        parentDiv.innerHTML = '';
+
+        for (let i = 0; i < frameCount; i++) {
+            const img = document.createElement('img');
+            img.src = imagesPathArray[i];
+            img.className = "animationFrame";
+            img.alt = "Frame " + i;
+            img.display = "none";
+            parentDiv.appendChild(img);
+        }
+    }
+
+    function loadAnimation(name) {
+        imagesPathArray = generateImagePathArray(name, frameCount);
+        addChildrenToParentDiv();
+        console.log("Loaded animation");
+    }
+
+    function playAnimation(){
+    
+    }
+
+    loadAnimation(avatarName);
+});
 
