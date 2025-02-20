78f08181abeedc65f476f75479ea1182d8a7b012
strobobrain <parkschaden@gmail.com>
2025-02-20 10:06:30 +0100

    added regions

--- Scripts/Animation/animation.js
+++ Scripts/Animation/animation.js
@@ -31,11 +31,13 @@ document.addEventListener('DOMContentLoaded', () => {
         for (let i = 0; i < frameCount; i++) {
             const img = document.createElement('img');
             img.src = pathArray[i];
+            // add class for css
             img.className = "animationFrame";
             img.alt = "Frame " + i;
             img.style.display = "none";
             parentDiv.appendChild(img);
         }
+        activateFirstchild(parentDiv);
     }
 
     function loadAnimation(name, parentName, frameCount) {
@@ -44,7 +46,7 @@ document.addEventListener('DOMContentLoaded', () => {
         let tempParentDiv = document.getElementById(parentName);
         addChildrenToParentDiv(tempParentDiv,imagesPathArray,frameCount);
 
-        playAnimation(tempParentDiv,1);
+
     }
 
     function changeVisibility(object){
@@ -55,19 +57,34 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    function activateFirstchild(object){
+        object.firstChild.style.display = "block";
+    }
+
     // Cycles through the children of the parent
 
     function playAnimation(parentDiv, framerate){
         let index = 0;
         let arrayLength = parentDiv.childNodes.length;
         setInterval(() => {
+            let nextIndex = (index + 1) % arrayLength;
+            changeVisibility(parentDiv.childNodes[nextIndex]);
             changeVisibility(parentDiv.childNodes[index]);
-            index = (index + 1) % arrayLength;
-            changeVisibility(parentDiv.childNodes[index]);
+            index = nextIndex;
         }, 1000 / framerate);
         console.log("Animation Played");
     }
+
+    function playAnimationWithId(idName, framerate){
+        let object = document.getElementById(idName);
+        playAnimation(object,framerate);
+    }
+
+
+
     loadAnimation("Crakuma_01","avatarParent_1",4);
+    playAnimationWithId("avatarParent_1",1);
 
 });
+
 
