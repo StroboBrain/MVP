b955f63e7a3a894f35da0b2b103aff4e28eed2ee
strobobrain <parkschaden@gmail.com>
2025-02-20 21:05:12 +0100

    refactor animation

--- Scripts/Animation/animation.js
+++ Scripts/Animation/animation.js
@@ -1,168 +1,154 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const maxFrames = 20;
-    var parentArray = document.querySelectorAll(".avatar");
-    const debug = true;
-    var debugLevel = 7;
-    var level = 0;
-    var currentPageName = getPageName();
-    var frameRate = 2.3;
+var maxFrames = 20;
+var debug = true;
+var debugLevel = 7;
+var level = 0;
+var frameRate = 2.3;
 
-    loadLevel();
+// Return the path to the frame with the name
+function generateFrameImagePath(folderName, name, index) {
+    let path = "ImageResources/Avatars/"+ folderName + "/" + name + "/frame_" + index + ".png";
+    return path;
+}
 
-    function loadLevel(){
-        level = localStorage.getItem(currentPageName) || 0;
-        // Option to debug
-        if (debugLevel) level = debugLevel;
-        if (debug) console.log("Current level set at " + level); 
+// Clean-Up Check frames earlier, max 20 frames
+function generateImagePathArray(folderName, name) {
+    const imagesArray = [];
+    for (let i = 0; i < maxFrames; i++) {
+        imagesArray.push(generateFrameImagePath(folderName, name, i));
     }
+    return imagesArray;
+}
+
+function loadLevel(currentPageName){
+    level = localStorage.getItem(currentPageName) || 0;
+    // Option to debug
+    if (debugLevel) level = debugLevel;
+    if (debug) console.log("Current level set at " + level); 
+}
 
 
-    function loadAvatar(numberOfAvatars){
-        if (numberOfAvatars>7) console.warn("more then 7 avatars requested");
-        for (let i = 0; i < numberOfAvatars+1; i++) {
-           
-        }
-    }
-
-    // Return the path to the frame with the name
-    function generateFrameImagePath(folderName, name, index) {
-        let path = "ImageResources/Avatars/"+ folderName + "/" + name + "/frame_" + index + ".png";
-        return path;
-    }
-
-    // Clean-Up Check frames earlier, max 20 frames
-    function generateImagePathArray(folderName, name) {
+function generateImagePathArray(folderName, name) {
         const imagesArray = [];
         for (let i = 0; i < maxFrames; i++) {
             imagesArray.push(generateFrameImagePath(folderName, name, i));
         }
-    
         return imagesArray;
+}
+
+function addChildrenToParentDiv(parentDiv, pathArray) {
+    if(parentDiv.firstChild){
+        console.log("Parent has a Child, no Image was added");
+        return;
     }
+    // Cleanup frameCount not needed anymore
+    
 
+    // Delete all children
+    parentDiv.innerHTML = '';
+    var breaking = false;
 
-    function addChildrenToParentDiv(parentDiv, pathArray,frameCount) {
-        if(parentDiv.firstChild){
-            console.log("Parent has a Child, no Image was added");
-            return;
+    for (let i = 0; i < maxFrames; i++) {
+        let tempPath = pathArray[i];
+        const img = document.createElement('img');
+        img.src = tempPath;
+
+        // Checks width to decide if a picture was loaded
+        if (img.naturalWidth<10){
+            if (debug) console.log("No more Frames");
+            breaking = true;
+            break;
         }
-        // Cleanup frameCount not needed anymore
-        
-
-        // Delete all children
-        parentDiv.innerHTML = '';
-
-        for (let i = 0; i < maxFrames; i++) {
-            let tempPath = pathArray[i];
-            const img = document.createElement('img');
-            img.src = tempPath;
-
-            // Checks width to decide if a picture was loaded
-            if (img.naturalWidth<10){
-                if (debug) console.log("No more Frames");
-                break;
-            }
-
-            // add classes for css
-            let className = "animationFrame " + parentDiv.id;
-
-            img.className = className;
-            img.alt = "Frame " + i;
-            img.style.display = "none";
-            parentDiv.appendChild(img);
-        }
-
-        //activateFirstchild(parentDiv);
+        // add classes for css
+        let className = "animationFrame " + parentDiv.id;
+        img.className = className;
+        img.alt = "Frame " + i;
+        img.style.display = "none";
+        parentDiv.appendChild(img);
+        if (breaking) console.log("still in loop");
     }
 
-    function loadAnimation(folderName, name, parentName) {
-        var imagesPathArray = generateImagePathArray(folderName, name);
-        let tempParentDiv = document.getElementById(parentName);
-        addChildrenToParentDiv(tempParentDiv,imagesPathArray);
-    }
+}
 
-    function changeVisibility(object){
-        if (object.style.display === "none") {
-            object.style.display = "block";
-        } else {
-            object.style.display = "none";
-        }
-    }
-
-    function isVisibil(object){
-        return object.style.display =="block";
-    }
-
-    function activateFirstchild(object){
-        object.firstChild.style.display = "block";
-    }
-
-    // Cycles through the children of the parent
-
-    function playAnimation(parentDiv, framerate){
-        let index = 0;
-        var arrayLength = parentDiv.childNodes.length;
-
-
-        setInterval(() => {
-            if (!parentDiv.firstChild){
-                loadAvatars();
-            }
-            let nextIndex = (index + 1) % arrayLength;
-            if (isVisibil(parentDiv.childNodes[index])){
-                changeVisibility(parentDiv.childNodes[index]);
-            }
-
-            changeVisibility(parentDiv.childNodes[nextIndex]);
-            index = nextIndex;
-        }, 1000 / framerate);
-        if (debug) console.log("Animation Played");
-    }
-
-
-    function playAnimationWithId(idName, framerate){
-        let object = document.getElementById(idName);
-        playAnimation(object,framerate);
-    }
-
-    function getPageName(){
-        // Get the full path of the current URL
-        let fullPath = window.location.pathname;
-        let pageName = fullPath.substring(fullPath.lastIndexOf('/') + 1);
-        pageName = pageName.split(".")[0];
-
-        if (debug) console.log("Current page :" + pageName);
-        return pageName;
-
-    }
-
-    // Starts the animations TODO improve
-
-    function loadAvatars(){
-        for (let i = 1; i <level+1; i++) {
-        let avatar = "avatar_" + i;
-        let avatarParentName = "avatarParent_"+i;
-        loadAnimation(currentPageName ,avatar, avatarParentName);
+function changeVisibility(object){
+    if (object.style.display === "none") {
+        object.style.display = "block";
+    } else {
+        object.style.display = "none";
     }
 }
-    loadAvatars();
 
-    
-    console.log("run");
+
+function isVisibil(object){
+    return object.style.display =="block";
+}
+
+
+function activateFirstchild(object){
+    object.firstChild.style.display = "block";
+}
+
+
+function loadAnimation(folderName, name, parentName) {
+    var imagesPathArray = generateImagePathArray(folderName, name);
+    let tempParentDiv = document.getElementById(parentName);
+    addChildrenToParentDiv(tempParentDiv,imagesPathArray);
+}
+
+function playAnimation(parentDiv, framerate){
+    let index = 0;
+    var arrayLength = parentDiv.childNodes.length;
+    setInterval(() => {
+        if (!parentDiv.firstChild){
+            loadAvatars();
+        }
+        let nextIndex = (index + 1) % arrayLength;
+        if (isVisibil(parentDiv.childNodes[index])){
+            changeVisibility(parentDiv.childNodes[index]);
+        }
+
+        changeVisibility(parentDiv.childNodes[nextIndex]);
+        index = nextIndex;
+    }, 1000 / framerate);
+
+    if (debug) console.log("Animation Played");
+}
+
+function playAnimationWithId(idName, framerate){
+    let object = document.getElementById(idName);
+    playAnimation(object,framerate);
+}
+
+function getPageName(){
+    // Get the full path of the current URL
+    let fullPath = window.location.pathname;
+    let pageName = fullPath.substring(fullPath.lastIndexOf('/') + 1);
+    pageName = pageName.split(".")[0];
+
+    if (debug) console.log("Current page :" + pageName);
+    return pageName;
+}
+
+// Starts the animations TODO improve
+function loadAvatars(currentPageName){
+    for (let i = 1; i <level+1; i++) {
+    let avatar = "avatar_" + i;
+    let avatarParentName = "avatarParent_"+i;
+    loadAnimation(currentPageName,avatar, avatarParentName);
+}
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    var currentPageName = getPageName();
+    loadLevel(currentPageName);
+    loadAvatars(currentPageName);
+});
+
+
+window.addEventListener('load', () => {
     for (let i = 1; i <level+1; i++) {
         let avatar = "avatar_" + i;
         let avatarParentName = "avatarParent_"+i;
         playAnimationWithId(avatarParentName, frameRate);
     }
-
 });
 
-window.addEventListener("load", () => {
-
-    for (let i = 1; i <level+1; i++) {
-        let avatar = "avatar_" + i;
-        let avatarParentName = "avatarParent_"+i;
-        playAnimationWithId(avatarParentName, frameRate);
-    }
-    console.log("late");
-});
