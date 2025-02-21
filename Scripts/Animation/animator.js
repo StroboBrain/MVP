97d2b788fbed79856112b02898fba9ccab8f4507
strobobrain <parkschaden@gmail.com>
2025-02-21 08:50:07 +0100

    animation restructur

--- Scripts/Animation/animator.js
+++ Scripts/Animation/animator.js
@@ -1,15 +1,45 @@
 var frameRate = 2.3;
+var loopArray = [];
+var mapTitle;
+var indexToToggle = 0;
+const constMaxFrames = 20;
 
 
-function runAnimation(){
+function runAnimationOfElement(){
     let elmentToToggle = "avatar_1_frame_0"
     toggleDisplay(elmentToToggle);
+}
+function runAnimation(){
+    cycleThrouLoopArray();
+}
+
+function activateFirstFrame(){
+    for (const item of loopArray){
+        toggleDisplayWithObject(item[0]);
+    }
+}
+
+
+
+function cycleThrouLoopArray(){
+
+    for (let i = 0; i<loopArray.length; i++) {
+        for (const item of loopArray) {
+        //Make sure its a valid index
+        let arrayIndex = indexToToggle%item.length;
+        let arrayIndexNext = (indexToToggle+1)%item.length;
+        toggleDisplayWithObject(item[arrayIndex]);
+        toggleDisplayWithObject(item[arrayIndexNext]);
+    }
+    }
+    indexToToggle++;
+    indexToToggle = indexToToggle%constMaxFrames;
+    
 }
 
 // Define the function to toggle the display style
 function toggleDisplay(elementClassId) {
     let tempIdObject = document.getElementById(elementClassId);
-    console.log(tempIdObject);
     toggleDisplayWithObject(tempIdObject);
 }
   
@@ -35,13 +65,23 @@ function loopFunction(framerate) {
     requestAnimationFrame(loop); // Start the loop
 }
 
+function generateLoopArray(){
 
+    for (let i = 1; i<8; i++) {
+        let tempLoopOnly = document.getElementsByClassName('avaParent_' + i);
+        loopArray.push(tempLoopOnly);
+    } 
+    
+}
 
-  
 
 
 window.onload = function() {
     console.log("Page has fully loaded.");
+    mapTitle = document.title;
+    console.log("Pagename: " + mapTitle);
+    generateLoopArray();
+    activateFirstFrame();
 
     // Add your code here
     loopFunction(frameRate);
