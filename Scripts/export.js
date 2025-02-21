db3a564513c3a0c3fef631659e6b3c1c12cbe9d2
StroboBrain <parkschaden@gmail.com>
2025-02-21 01:39:55 +0100

    cleaned main

--- Scripts/export.js
+++
@@ -1,8 +0,0 @@
-function exportDivToJPEG() {
-    html2canvas(document.getElementById('avatarDiv')).then(canvas => {
-        var link = document.createElement('a');
-        link.download = 'avatar.jpeg';
-        link.href = canvas.toDataURL('image/jpeg');
-        link.click();
-    });
-}
